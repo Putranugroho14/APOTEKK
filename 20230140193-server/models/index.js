25082f6c62b5a8b7b534ff4b493952f09d4d538e
Putranugroho14 <putranugroho61@gmail.com>
2025-12-24 17:02:16 +0700

    updt

--- 20230140193-server/models/index.js
+++ 20230140193-server/models/index.js
@@ -1,22 +1,52 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
-
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  dialectModule: require('mysql2'),
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true,
+let sequelize;
+if (config.use_env_variable) {
+  // Untuk production, parse DATABASE_URL secara manual
+  const dbUrl = process.env[config.use_env_variable];
+  
+  sequelize = new Sequelize(dbUrl, {
+    dialect: 'mysql',
+    dialectModule: require('mysql2'), // ← TAMBAHKAN INI
+    dialectOptions: config.dialectOptions || {},
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     }
-  },
-  logging: false,
-});
+  });
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    dialectModule: require('mysql2') // ← TAMBAHKAN INI JUGA
+  });
+}
 
-// IMPORT MANUAL: Pastikan file 'obat.js' ada di folder yang sama
-db.obat = require('./obat')(sequelize, Sequelize.DataTypes);
+// ... sisa code sama
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
--- 20230140193-server/models/obat.js
+++ 20230140193-server/models/obat.js
@@ -1,11 +1,30 @@
-const db = require('../models/index');
-const modelObat = db.obat; // Pastikan menggunakan db.obat
-
-export default async function handler(req, res) {
-  try {
-    const data = await modelObat.findAll();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class obat extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
   }
-}
+  obat.init({
+    nama_obat: DataTypes.STRING,
+    deskripsi: DataTypes.TEXT,
+    stok: DataTypes.INTEGER,
+    harga: DataTypes.DECIMAL,
+    gambar_url: DataTypes.STRING,
+    kategori: DataTypes.STRING,
+    authorId: DataTypes.INTEGER,
+    is_published: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'obat',
+  });
+  return obat;
+};
