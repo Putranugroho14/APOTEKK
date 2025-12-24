bf07422c543dec4f6de7a96d426d0be7596b567d
Putranugroho14 <putranugroho61@gmail.com>
2025-12-24 16:53:01 +0700

    update

--- 20230140193-server/models/obat.js
+++ 20230140193-server/models/obat.js
@@ -1,30 +1,19 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class obat extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+const db = require('../models/index'); // Sesuaikan path ke folder models
+const Obat = db.obat; // GUNAKAN HURUF KECIL sesuai modelName di file Anda
+
+export default async function handler(req, res) {
+  try {
+    // Pastikan database terkoneksi
+    await db.sequelize.authenticate();
+    
+    // Gunakan variabel Obat (huruf kecil)
+    const data = await Obat.findAll(); 
+    
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ 
+      error: error.message,
+      stack: error.stack // Ini akan membantu kita debug lebih dalam di browser
+    });
   }
-  obat.init({
-    nama_obat: DataTypes.STRING,
-    deskripsi: DataTypes.TEXT,
-    stok: DataTypes.INTEGER,
-    harga: DataTypes.DECIMAL,
-    gambar_url: DataTypes.STRING,
-    kategori: DataTypes.STRING,
-    authorId: DataTypes.INTEGER,
-    is_published: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'obat',
-  });
-  return obat;
-};
+}
