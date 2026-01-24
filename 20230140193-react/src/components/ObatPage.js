cf693f85e2812276384cbaa9f6cdb64512194bed
Putranugroho14 <putranugroho61@gmail.com>
2026-01-24 12:36:05 +0700

    Fix: Add missing ChevronDown import in ObatPage.js

--- 20230140193-react/src/components/ObatPage.js
+++ 20230140193-react/src/components/ObatPage.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import {
     Trash2, Plus, X, Package,
     Search, LayoutDashboard, RefreshCw,
-    FileText, LogOut, Save, Eye, EyeOff, Menu, Upload, ShoppingCart
+    FileText, LogOut, Save, Eye, EyeOff, Menu, Upload, ShoppingCart, ChevronDown
 } from 'lucide-react';
 import { useNavigate } from "react-router-dom";
 import API_BASE_URL from "../config";
